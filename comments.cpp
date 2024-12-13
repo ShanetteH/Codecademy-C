c689e4037ead502c8fa08b272ea10c4ed17d796b
Nettie <77640550+ShanetteH@users.noreply.github.com>
2024-12-13 18:42:40 -0500

    Different types of comments

---
+++ comments.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+// This is a single line comment
+
+/* This is a 
+multi-line 
+comment */
+
+// Prints "hi!" to the terminal
+std::cout << "hi!";
+
+std::cout << "hi!"; // Prints "hi!"
+
+/* This is all commented.
+std::cout << "hi!"; 
+None of this is going to run*/
+
+}
