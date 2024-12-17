1e2eacc32e177f752729be6f177dffecc4f1424a
Nettie <77640550+ShanetteH@users.noreply.github.com>
2024-12-17 11:24:12 -0500

    Declaring and initializing variables

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    // Declare a variable
+    int score;
+
+    // Initialize a variable
+    score = 0;
+
+    // Declare and Initialize a variable
+    int year = 2024;
+
+
+    return 0; 
+}
