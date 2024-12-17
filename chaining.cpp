88b227072650fd52f4854938b13722ef54588403
Nettie <77640550+ShanetteH@users.noreply.github.com>
2024-12-17 11:26:26 -0500

    Chaining cout with variables

---
+++ chaining.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    int age = 23;
+
+    std::cout << "Hello, I am ";
+    std::cout << age;
+    std::cout << " years old\n";
+
+
+    std::cout << "Hello, I am " << age << " years old\n"; //This is called chaining
+
+  
+  int score = 0;
+  
+  // Output
+  
+  std::cout << "Player score:" << score << "\n";
+
+   
+}
