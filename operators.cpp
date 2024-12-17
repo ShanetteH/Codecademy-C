1c872178c18700276f837d6bcc5fd4e8e6365af8
Nettie <77640550+ShanetteH@users.noreply.github.com>
2024-12-17 11:25:04 -0500

    Using arithmetic operators

---
+++ operators.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(){
+
+    int score = 0;
+    // score is 0
+
+    score = 4 + 2;
+    // it is now 6
+
+    score = 4 - 2;
+    // it is now 2
+
+    score = 4 * 2;
+    // it is now 8
+
+    score = 4 / 2;
+    // and now 2
+
+    score = 5 % 2;
+    // and now 1
+
+    score = 1234 * 99;
+  
+    std::cout << score << "\n";
+
+}
